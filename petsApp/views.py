99338e6abebc52cac51c3a6683f4b87df2d26e88
unknown <2889991R@student.gla.ac.uk>
2024-03-21 21:45:14 +0000

    Added in css

--- petsApp/views.py
+++ petsApp/views.py
@@ -3,12 +3,12 @@ from petsApp.models import Student, Cats
 
 
 def index(request):
-    student = Student.objects.all()
-    cats = Cats.objects.all()
+    student = Student.objects.all().order_by('firstName')
+    cats = Cats.objects.all().order_by('name')
 
     return render(request, 'petsApp/index.html', {'student': student, 'cats': cats})
 
 
 def pets(request):
-    cats = Cats.objects.all()
+    cats = Cats.objects.all().order_by('name')
     return render(request, 'petsApp/pets.html', {'cats': cats})
