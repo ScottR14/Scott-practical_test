e2f71ac1e5600c8e97d507bca6851cbb999021d3
unknown <2889991R@student.gla.ac.uk>
2024-03-21 23:02:17 +0000

    python anywhere

--- pets/wsgi.py
+++ pets/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for pets project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/GA/Scott-practical_test/pets/'
+if path not in sys.path:
+    sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pets.settings')
+os.chdir(path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'pets.settings')
+# Set up Django-- let it instantiate everything!
+import django
+django.setup()
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler
